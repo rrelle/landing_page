cffce14ec982914b26a78c4362a451ea6547a7a5
Rrelle <rrelleferguson@usf.edu>
2026-03-08 22:53:43 -0400

    added java

---
+++ cc_7.js
@@ -0,0 +1,9 @@
+const input = [document.getElementById('heroInput')];
+const button = document.getElementById('updateButton');
+const headline = document.getElementById('ctaHeadline');
+button.addEventListener('click', () => {
+    const newText = input[0].value;
+    if (newText) {
+        headline.textContent = newText;
+    }
+    
